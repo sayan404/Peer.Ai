62d236d5c63882cbc0d2b34478767fc8e123cdd7
Sayan Majumder <95490367+sayan404@users.noreply.github.com>
2023-07-16 13:58:01 +0530

    Update background.js

--- src/static/background.js
+++ src/static/background.js
@@ -74,7 +74,7 @@ function getPageContent() {
 }
 
 async function makeAPIRequest(modeActivation) {
-    const apiKey = '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';
+    const apiKey = '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';
     const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
     console.log("ok111");
     const prompt = modeActivation;
@@ -370,7 +370,7 @@ async function closeTabs(tabId) {
 
 // Function to make the API request
 async function makeAPIRequest(modeActivation) {
-    const apiKey = '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';
+    const apiKey = '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';
     const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
     console.log("ok111");
     const prompt = modeActivation;
